5074bb00038ecd17c0e5be33ad39a0a17b73f8d6
ReGeorged <giorgi.bazierashvili@gmail.com>
2024-01-21 13:59:34 -0800

    added onError Allure screenshot

--- wdio.conf.js
+++ wdio.conf.js
@@ -248,8 +248,11 @@ export const config = {
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+        if (error) {
+            await driver.takeScreenshot();
+        }
+    },
 
 
     /**
